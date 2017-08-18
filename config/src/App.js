c1139624ffcd5b3451b2aa04b957aa0012e3cd98
849203846@qq.com <849203846@qq.com>
2017-08-18 18:39:12 +0800

    a

---
+++ config/src/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+import './css/index.less'
+export default class App extends Component{
+    render(){
+        return(
+            <div className="ceshi">
+                aaaaaaaaa
+            </div>
+        )
+    }
+}
